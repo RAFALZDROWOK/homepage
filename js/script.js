610b0ee429b89c1d6894c4c262ab44dc996c8103
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-01-14 12:29:40 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log("Cześć!");
+
+
+let button = document.querySelector(".button");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".themeName");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("grayBackground");
+
+    themeName.innerText = body.classList.contains("grayBackground") ? "jasny" : "ciemny"; 
+});
+
+/*   operatorwarunkowy potrójny "ternary" (krótka i wygodna składnia)
+
+warunek ? jeśliSpełniony() : jeśliNieSpełniony()   */
+
+
+
